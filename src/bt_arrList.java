eb63717a028f7d0086a42bdded34b0d25e9720b8
vuductai2003 <vuhech@gmail.com>
2023-05-27 12:59:20 +0700

    mới

---
+++ src/bt_arrList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class bt_arrList {
+    public static void main(String[] args) {
+        /*viết ct tạo ra 1 liisst có n phần tử,
+        n nhập từ bàn phím
+        , các pt là số ngẫu nhiên từ (1, 100)*/
+        Random rd = new Random();
+        Scanner sc = new Scanner (System.in);
+        System.out.println("nhập s lượng phần tử: ");
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int ngauNhien = rd.nextInt(1,101);
+            arr.add(ngauNhien);
+        }
+        System.out.println(arr);
+    }
+}
--- src/gfhf.java
+++
@@ -1,7 +0,0 @@
-public class gfhf {
-    public static void main(String[] args) {
-        for(int i = 1; i <= 10000;i++){
-            System.out.printf("%d. haha\n ",i);
-        }
-    }
-}
